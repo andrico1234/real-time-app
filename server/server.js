2cecc0f8bc3d2627b6bfd7bbeba7a9bf93433f45
Andrico <andrico1234@yahoo.co.uk>
2017-05-07 01:21:54 +0400

    add socket.io to project

--- server/server.js
+++ server/server.js
@@ -1,14 +1,29 @@
+"use strict";
+
 const express = require('express');
-const hbs = require('handlebars');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname + '/../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+
+    console.log("new user connected");
+
+    socket.on('disconnect', () => {
+
+        console.log('disconnected');
+    });
+});
+
+server.listen(port, () => {
 
     console.log(`Server is up on port ${port}`);
 });
