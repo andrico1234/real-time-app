b335c76ebb467b45e83536fff3d8e0db7e2b55d1
Andrico <andrico1234@yahoo.co.uk>
2017-05-25 23:24:02 +0100

    messages only send to right rooms

--- server/server.js
+++ server/server.js
@@ -44,14 +44,24 @@
 
         socket.on('createMessage', (message, callback) => {
 
-            console.log('createMessage', message);
-            io.emit('newMessage', generateMessage(message.from, message.text));
+            var user = users.getUser(socket.id);
+
+            if (user && isRealString(message.text)) {
+
+                io.to(user[0].room).emit('newMessage', generateMessage(user[0].name, message.text));
+            }
+
             callback();
         });
 
         socket.on('createLocationMessage', (coords) => {
 
-            io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+            var user = users.getUser(socket.id);
+
+            if (user) {
+
+                io.to(user[0].room).emit('newLocationMessage', generateLocationMessage(user[0].name, coords.latitude, coords.longitude));
+            }
         });
 
         socket.on('disconnect', () => {
